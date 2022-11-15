9ecb7cc3684a07a171eef477b2cfcc666aaf083b
Dustin Shunta <dustinhans@gmail.com>
2022-11-15 10:19:30 -0500

    Updated TeleOp for cone delivery tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TEST_DELIVERY_DISTANCE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TEST_DELIVERY_DISTANCE.java
@@ -3,13 +3,10 @@ package org.firstinspires.ftc.teamcode.OpModes;
 import static org.firstinspires.ftc.teamcode.ObjectClasses.Arm.ARM_CENTER_INTAKE;
 import static org.firstinspires.ftc.teamcode.ObjectClasses.Arm.ARM_LEFT_OUTTAKE;
 import static org.firstinspires.ftc.teamcode.ObjectClasses.Arm.ARM_RIGHT_OUTTAKE;
-import static org.firstinspires.ftc.teamcode.ObjectClasses.DriveTrain.LOW_SPEED;
-import static org.firstinspires.ftc.teamcode.ObjectClasses.GameConstants.FIVE_CONE_STACK_INTAKE_HEIGHT_ENC_VAL;
+import static org.firstinspires.ftc.teamcode.ObjectClasses.GameConstants.HALF_TILE_DISTANCE;
 import static org.firstinspires.ftc.teamcode.ObjectClasses.GameConstants.HIGH_CONE_JUNCTION_SCORE_HEIGHT_ENC_VAL;
 import static org.firstinspires.ftc.teamcode.ObjectClasses.GameConstants.MEDIUM_CONE_JUNCTION_SCORE_HEIGHT_ENC_VAL;
-import static org.firstinspires.ftc.teamcode.ObjectClasses.GameConstants.QUARTER_TILE_DISTANCE;
-import static org.firstinspires.ftc.teamcode.ObjectClasses.GameConstants.SIXTEENTH_TILE_DISTANCE;
-import static org.firstinspires.ftc.teamcode.ObjectClasses.GameConstants.THIRTYSECOND_TILE_DISTANCE;
+import static org.firstinspires.ftc.teamcode.ObjectClasses.GameConstants.ONE_CONE_INTAKE_HEIGHT_ENC_VAL;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -43,10 +40,17 @@ public class TEST_DELIVERY_DISTANCE extends LinearOpMode {
     private final ElapsedTime runtime = new ElapsedTime();
 
 
+    int delay_before_claw_open = 200;
+    int delay_after_claw_open = 200;
+
     Gamepad currentGamepad1 = new Gamepad();
     Gamepad previousGamepad1 = new Gamepad();
     Gamepad currentGamepad2 = new Gamepad();
     Gamepad previousGamepad2 = new Gamepad();
+
+    double test_stafe_into_pole_distance = HALF_TILE_DISTANCE;
+    double test_speed;
+    int delivery_side = 1; //1 is right side, -1 is left side
 
     @Override
     public void runOpMode() {
@@ -69,89 +73,141 @@ public class TEST_DELIVERY_DISTANCE extends LinearOpMode {
         sleep(1000);
 
         while (!isStarted()) {
-            //save current and previous gamepad values for one loop
-            previousGamepad1 = BConfig.copy(currentGamepad1);
-            currentGamepad1 = BConfig.copy(gamepad1);
-
-            previousGamepad2 = BConfig.copy(currentGamepad2);
-            currentGamepad2 = BConfig.copy(gamepad2);
-
-            //Use Webcam to find out Signal using April Tags and save in currentSignal
-            Vision.CheckForAprilTags(this);
-
-            // User sets starting location left or right, and confirms selection with a button press
-            // LEFT is a multiplier of 1, RIGHT is a multiplier of -1
-            BConfig.ConfigureStartingPosition(currentGamepad1.dpad_left, previousGamepad1.dpad_left,
-                    currentGamepad1.dpad_right, previousGamepad1.dpad_right,
-                    currentGamepad1.b, previousGamepad1.b);
-
-            telemetry.addData("Signal is ", Vision.currentSignal);
-            telemetry.addLine(" ");
-            telemetry.addLine("Select Starting Position with D-pad");
-            telemetry.addData("Current Starting Position ", ButtonConfig.currentStartPosition);
-            if (ButtonConfig.confirmStartingPositionSelection == false) {
-                telemetry.addData("Unlocked", "Press B to lock selection");
-            } else {
-                telemetry.addData("Locked", "Press B to unlock selection");
-            }
+            telemetry.addLine("This program is to tune cone delivery");
             telemetry.update();
-
-            sleep(20);
         }
 
+        ServoClaw.toggleClaw(); // open the claw to start
         runtime.reset();
         Gyro.init(hardwareMap);
 
-        //Use this code to just test if we can score by strafing into a pole
+        //Use this code to tune the speed and distance to deliver a cone onto a pole
+        while (opModeIsActive()) {
 
-        telemetry.addData("Signal is ", Signal);
-        telemetry.addData("Selected Starting Position ", ButtonConfig.currentStartPosition);
-        telemetry.update();
+            //Store the previous loop's gamepad values.
+            previousGamepad1 = BConfig.copy(currentGamepad1);
+            previousGamepad2 = BConfig.copy(currentGamepad2);
 
-        Lift.StartLifting(MEDIUM_CONE_JUNCTION_SCORE_HEIGHT_ENC_VAL);
-        while (opModeIsActive() && (Lift.alreadyLifting)) {
-            Lift.ContinueLifting();
+            //Store the gamepad values to be used for this iteration of the loop.
+            currentGamepad1 = BConfig.copy(gamepad1);
+            currentGamepad2 = BConfig.copy(gamepad2);
+
+            //Change speed by .05 power increments
+            if (currentGamepad1.dpad_up && !previousGamepad1.dpad_up) {
+                test_speed += .05;
+                if (test_speed >= 1) test_speed = 1;
+            }
+
+            if (currentGamepad1.dpad_down && !previousGamepad1.dpad_down) {
+                test_speed -= .05;
+                if (test_speed <= 0) test_speed = 0;
+            }
+
+            //Change distance in .25 inch increments
+            if (currentGamepad1.dpad_right && !previousGamepad1.dpad_right) {
+                test_stafe_into_pole_distance += .25;
+                if (test_stafe_into_pole_distance >= 30) test_stafe_into_pole_distance = 30;
+            }
+
+            if (currentGamepad1.dpad_left && !previousGamepad1.dpad_left) {
+                test_stafe_into_pole_distance -= .25;
+                if (test_stafe_into_pole_distance <= 5) test_stafe_into_pole_distance = 5;
+            }
+
+            //Change delivery side with left stick
+            if (currentGamepad1.left_stick_x > .1 && previousGamepad1.left_stick_x < .1) {
+                //deliver to the left side
+                delivery_side = -1;
+            }
+
+            if (currentGamepad1.left_stick_x < -.1 && previousGamepad1.left_stick_x > -.1) {
+                //deliver to the right side
+                delivery_side = 1;
+            }
+
+            //Change delay before opening claw with right stick (L/R)
+            if (currentGamepad1.right_stick_x > .1 && previousGamepad1.right_stick_x < .1) {
+                //deliver to the left side
+                delay_before_claw_open += 50;
+                if (delay_before_claw_open >= 1000) delay_before_claw_open = 1000;
+
+            }
+
+            if (currentGamepad1.right_stick_x < -.1 && previousGamepad1.right_stick_x > -.1) {
+                //deliver to the right side
+                delay_before_claw_open -= 50;
+                if (delay_before_claw_open <= 50) delay_before_claw_open = 50;
+            }
+
+            //Change delay after opening claw with right stick (L/R)
+            if (currentGamepad1.right_stick_y > .1 && previousGamepad1.right_stick_y < .1) {
+                //deliver to the left side
+                delay_after_claw_open += 50;
+                if (delay_after_claw_open >= 1000) delay_after_claw_open = 1000;
+
+            }
+
+            if (currentGamepad1.right_stick_y < -.1 && previousGamepad1.right_stick_y > -.1) {
+                //deliver to the right side
+                delay_after_claw_open -= 50;
+                if (delay_after_claw_open <= 50) delay_after_claw_open = 50;
+            }
+
+            //reset to try again
+            //close claw on cone, raise lift to a starting height
+            if (currentGamepad1.x && !previousGamepad1.x) {
+                ServoClaw.toggleClaw();
+                Lift.StartLifting(MEDIUM_CONE_JUNCTION_SCORE_HEIGHT_ENC_VAL);
+            }
+
+            if (currentGamepad1.a && !previousGamepad1.a) {
+                //1 = RIGHT SIDE
+                //-1 = LEFT SIDE
+                Deliver_and_Return(delivery_side);
+            }
+
+            telemetry.addLine("Use D-Pad to change test speed (up/down) and test strafe distance into pole (left/right)");
+            telemetry.addLine("Change delivery side with left stick (left/right)");
+            telemetry.addLine("Change delay before opening claw (left/right) and delay after opening claw (up/down) in milliseconds");
+            telemetry.addLine("Press X button to close claw and raise lift to safe driving height");
+            telemetry.addLine("Press A button to strafe test_distance into pole and drop off cone at test_speed");
+            telemetry.addData("Test Strafe Distance", test_stafe_into_pole_distance);
+            telemetry.addData("Test Speed", test_speed);
+            telemetry.addData("Side: 1 is right, -1 is left", delivery_side);
+            telemetry.addData("Delay before opening claw", delay_before_claw_open);
+            telemetry.addData("Delay after opening claw", delay_after_claw_open);
+            telemetry.update();
+
         }
+    }
 
-        if ((ButtonConfig.currentStartPosition == ButtonConfig.StartingPosition.RIGHT_SIDE)) {
+    public void Deliver_and_Return(int side) {
+        if (side==1) {
             ServoArm.setPosition(ARM_RIGHT_OUTTAKE);
         } else ServoArm.setPosition(ARM_LEFT_OUTTAKE);
 
         //Strafe close to High Pole
         Lift.StartLifting(HIGH_CONE_JUNCTION_SCORE_HEIGHT_ENC_VAL);
-        MecDrive.startStrafeDrive(LOW_SPEED, -(QUARTER_TILE_DISTANCE + SIXTEENTH_TILE_DISTANCE+THIRTYSECOND_TILE_DISTANCE) * ButtonConfig.startPositionMultiplier);
+        MecDrive.startStrafeDrive(test_speed, test_stafe_into_pole_distance*side);
         while (opModeIsActive() && MecDrive.alreadyStrafing == true) {
             MecDrive.ContinueStrafing();
         }
 
-        //If on the right side, need to back off just a little
-        /*
-        if (ButtonConfig.currentStartPosition == ButtonConfig.StartingPosition.RIGHT_SIDE) {
-            MecDrive.startStrafeDrive(LOW_SPEED, (SIXTEENTH_TILE_DISTANCE) * ButtonConfig.startPositionMultiplier);
-            while (opModeIsActive() && MecDrive.alreadyStrafing == true) {
-                MecDrive.ContinueStrafing();
-            }
-        }
-        */
-
-        sleep(200);
+        sleep(delay_before_claw_open);
         //Open claw to drop cone
         ServoClaw.toggleClaw();
-        sleep(400);
+        sleep(delay_after_claw_open);
 
-            //Strafe away from High Pole
-            MecDrive.startStrafeDrive(LOW_SPEED, QUARTER_TILE_DISTANCE * ButtonConfig.startPositionMultiplier);
-            while (opModeIsActive() && MecDrive.alreadyStrafing == true) {
-                MecDrive.ContinueStrafing();
-            }
+        //Strafe away from High Pole
+        MecDrive.startStrafeDrive(test_speed, -test_stafe_into_pole_distance*side);
+        while (opModeIsActive() && MecDrive.alreadyStrafing == true) {
+            MecDrive.ContinueStrafing();
+        }
 
-            //close the claw
-            ServoClaw.toggleClaw();
-            ServoArm.setPosition(ARM_CENTER_INTAKE);
-            Lift.StartLifting(FIVE_CONE_STACK_INTAKE_HEIGHT_ENC_VAL);
-
-      }
+        ServoArm.setPosition(ARM_CENTER_INTAKE);
+        Lift.StartLifting(ONE_CONE_INTAKE_HEIGHT_ENC_VAL);
     }
+}
 
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TEST_DRIVE_AND_STRAFE_DISTANCE_TUNING.java
@@ -0,0 +1,146 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import static org.firstinspires.ftc.teamcode.ObjectClasses.DriveTrain.LOW_SPEED;
+import static org.firstinspires.ftc.teamcode.ObjectClasses.GameConstants.FULL_TILE_DISTANCE;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.ObjectClasses.AprilTagVision;
+import org.firstinspires.ftc.teamcode.ObjectClasses.Arm;
+import org.firstinspires.ftc.teamcode.ObjectClasses.ButtonConfig;
+import org.firstinspires.ftc.teamcode.ObjectClasses.Claw;
+import org.firstinspires.ftc.teamcode.ObjectClasses.DriveTrain;
+import org.firstinspires.ftc.teamcode.ObjectClasses.Gyro;
+import org.firstinspires.ftc.teamcode.ObjectClasses.Intake;
+import org.firstinspires.ftc.teamcode.ObjectClasses.Lift;
+
+
+@TeleOp(name = "TEST_DRIVE_AND_STRAFE_DISTANCE_TUNING")
+public class TEST_DRIVE_AND_STRAFE_DISTANCE_TUNING extends LinearOpMode {
+
+    int Signal;
+    DriveTrain MecDrive = new DriveTrain(this);
+    ButtonConfig BConfig = new ButtonConfig(this);
+
+    Intake ServoIntake = new Intake();
+    AprilTagVision Vision = new AprilTagVision();
+    Claw ServoClaw = new Claw();
+    Lift Lift = new Lift(this);
+    Arm ServoArm = new Arm(Lift, ServoIntake, ServoClaw, this);
+    Gyro Gyro = new Gyro(this);
+
+    double test_distance = FULL_TILE_DISTANCE;
+    double test_speed = LOW_SPEED;
+
+    private final ElapsedTime runtime = new ElapsedTime();
+
+
+    Gamepad currentGamepad1 = new Gamepad();
+    Gamepad previousGamepad1 = new Gamepad();
+    Gamepad currentGamepad2 = new Gamepad();
+    Gamepad previousGamepad2 = new Gamepad();
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+
+        MecDrive.init(hardwareMap);
+        ServoIntake.init(hardwareMap);
+        ServoClaw.init(hardwareMap);
+        Lift.init(hardwareMap);
+        ServoArm.init(hardwareMap);
+        Gyro.init(hardwareMap);
+        Vision.init(hardwareMap);
+        BConfig.init();
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        sleep(1000);
+
+        while (!isStarted()) {
+            telemetry.addLine("This program is just for tuning driving and strafing distance accuracy");
+            telemetry.update();
+            sleep(20);
+        }
+
+        runtime.reset();
+        Gyro.init(hardwareMap);
+
+        while (opModeIsActive()) {
+
+            //Store the previous loop's gamepad values.
+            previousGamepad1 = BConfig.copy(currentGamepad1);
+            previousGamepad2 = BConfig.copy(currentGamepad2);
+
+            //Store the gamepad values to be used for this iteration of the loop.
+            currentGamepad1 = BConfig.copy(gamepad1);
+            currentGamepad2 = BConfig.copy(gamepad2);
+
+            //Change speed by .05 power increments
+            if (currentGamepad1.dpad_up && !previousGamepad1.dpad_up){
+                test_speed += .05;
+                if (test_speed >= 1) test_speed =1;
+            }
+
+            if (currentGamepad1.dpad_up && !previousGamepad1.dpad_up){
+                test_speed -= .05;
+                if (test_speed <= 0) test_speed =0;
+            }
+
+            //Change distance in .25 inch increments
+            if (currentGamepad1.dpad_right && !previousGamepad1.dpad_right){
+                test_distance += .25;
+                if (test_distance >= 50) test_distance =50;
+            }
+
+            if (currentGamepad1.dpad_left && !previousGamepad1.dpad_left){
+                test_distance -= .25;
+                if (test_distance <= 5) test_distance = 5;
+            }
+
+            if (currentGamepad1.y && !previousGamepad1.y) {
+                //Use this code to test driving a tile distance
+                //Might want to test using ORs instead of ANDs for the encoder finish distance
+                MecDrive.startEncoderDrive(test_speed, test_distance);
+            }
+
+            if (currentGamepad1.a && !previousGamepad1.a) {
+                //Use this code to test driving a tile distance
+                //Might want to test using ORs instead of ANDs for the encoder finish distance
+                MecDrive.startEncoderDrive(test_speed, -test_distance);
+            }
+
+            if (currentGamepad1.b && !previousGamepad1.b) {
+                //Use this code to test strafing a tile distance
+                //Might want to test using ORs instead of ANDs for the encoder finish distance
+                MecDrive.startEncoderDrive(test_speed, test_distance);
+            }
+
+
+            if (currentGamepad1.x && !previousGamepad1.x) {
+                //Use this code to test strafing a tile distance
+                //Might want to test using ORs instead of ANDs for the encoder finish distance
+                MecDrive.startEncoderDrive(test_speed, -test_distance);
+            }
+
+            //Automated tasks (driving, turning, strafing, vision strafing, auto deliver)
+            MecDrive.ContinueAutomaticTasks(Gyro, ServoArm, Lift, ServoClaw, ServoIntake);
+
+            telemetry.addLine("Use D-Pad to change test speed (left/right) and test distance (up/down)");
+            telemetry.addLine("Press Y/A button to drive forward/back test_distance at test_speed");
+            telemetry.addLine("Press X/B button to strafe left/right test_distance at test_speed");
+            telemetry.addData("Test Distance", test_distance);
+            telemetry.addData("Test Speed", test_speed);
+            telemetry.update();
+        }
+      }
+    }
+
+
+
